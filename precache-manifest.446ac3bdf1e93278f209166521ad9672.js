06e150b38034391351e4d114b2420a1769b7fa0a
WaterLeung25 <waterleung25@gmail.com>
2019-02-13 16:31:44 +0100

    Updates

---
+++ precache-manifest.446ac3bdf1e93278f209166521ad9672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1bacfd9018e1105f818",
+    "url": "/todoList-repo/static/js/runtime~main.d1bacfd9.js"
+  },
+  {
+    "revision": "ea875877dda5a4130360",
+    "url": "/todoList-repo/static/js/main.ea875877.chunk.js"
+  },
+  {
+    "revision": "058ec648b8c83eea3b86",
+    "url": "/todoList-repo/static/js/1.058ec648.chunk.js"
+  },
+  {
+    "revision": "ea875877dda5a4130360",
+    "url": "/todoList-repo/static/css/main.4b343c6c.chunk.css"
+  },
+  {
+    "revision": "c15cff56dfc81b531e1645b04a7219be",
+    "url": "/todoList-repo/index.html"
+  }
+];
